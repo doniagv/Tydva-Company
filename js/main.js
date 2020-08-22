e3b70348e5a9c960586530945a21617c281a6289
Javier González <doniagv@gmail.com>
2020-08-22 12:43:56 -0500

    Scroll reveal effect added

---
+++ js/main.js
@@ -0,0 +1,31 @@
+ScrollReveal().reveal(".container", { delay: 500, reset: true });
+ScrollReveal().reveal(".container-fluid", { delay: 500, reset: true });
+// ScrollReveal().reveal(".servicios", { delay: 400, reset: true });
+ScrollReveal().reveal(".animate-left", {
+  origin: "left",
+  duration: 1000,
+  distance: "20rem",
+  delay: 300,
+  reset: true,
+});
+ScrollReveal().reveal(".animate-right", {
+  origin: "right",
+  duration: 1000,
+  distance: "20rem",
+  delay: 300,
+  reset: true,
+});
+ScrollReveal().reveal(".animate-top", {
+  origin: "top",
+  duration: 1000,
+  distance: "20rem",
+  delay: 300,
+  reset: true,
+});
+ScrollReveal().reveal(".animate-bottom", {
+  origin: "bottom",
+  duration: 1000,
+  distance: "20rem",
+  delay: 300,
+  reset: true,
+});
