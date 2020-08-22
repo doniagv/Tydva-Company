94ebfc718dcaaf1c2537af047c9428873e916c05
Javier González <doniagv@gmail.com>
2020-08-22 13:19:28 -0500

    distance decreased

--- js/main.js
+++ js/main.js
@@ -3,28 +3,28 @@ ScrollReveal().reveal(".normal", { delay: 500, reset: true, duration: 500 });
 ScrollReveal().reveal(".animate-left", {
   origin: "left",
   duration: 1000,
-  distance: "20rem",
+  distance: "10rem",
   delay: 300,
   reset: true,
 });
 ScrollReveal().reveal(".animate-right", {
   origin: "right",
   duration: 1000,
-  distance: "20rem",
+  distance: "10rem",
   delay: 300,
   reset: true,
 });
 ScrollReveal().reveal(".animate-top", {
   origin: "top",
   duration: 1000,
-  distance: "20rem",
+  distance: "10rem",
   delay: 300,
   reset: true,
 });
 ScrollReveal().reveal(".animate-bottom", {
   origin: "bottom",
   duration: 1000,
-  distance: "20rem",
+  distance: "10rem",
   delay: 300,
   reset: true,
 });
